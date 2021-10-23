49304c69ef0d9b2d546e080219c93523fd7878dc
Cody LeClair <cody.leclair.us@protonmail.com>
2021-10-23 15:40:23 -0400

    Initial commit

---
+++ src/main/java/com/example/continuousdelivery/ContinuousDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.continuousdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/continuousdelivery/ContinuousDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.continuousdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuousDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
