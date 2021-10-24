207cf336fbe0d0a99177e6bfe4ce03d9ef66c951
Cody LeClair <cody.leclair.us@protonmail.com>
2021-10-24 09:43:49 -0400

    Added web attributes

--- src/main/java/com/example/continuousdelivery/ContinuousDeliveryApplication.java
+++ src/main/java/com/example/continuousdelivery/ContinuousDeliveryApplication.java
@@ -1,9 +1,14 @@
 package com.example.continuousdelivery;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class ContinuousDeliveryApplication {
 
 	@RequestMapping("/")
