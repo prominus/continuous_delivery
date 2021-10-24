1d12e25cc73ac8263d31093bf0057047464a6fb7
Cody LeClair <cody.leclair.us@protonmail.com>
2021-10-24 09:32:51 -0400

    Added string web request

--- src/main/java/com/example/continuousdelivery/ContinuousDeliveryApplication.java
+++ src/main/java/com/example/continuousdelivery/ContinuousDeliveryApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContinuousDeliveryApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Continuous Delivery";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ContinuousDeliveryApplication.class, args);
 	}
